d77bf5801d8654cd2e28821cd3f60a1c250669a1
stark1996 <stevenfernz96@gmail.com>
2020-12-01 11:18:20 +0530

    build folder added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import _ from 'lodash';
+import icon from './John_1.png';
 
 function component() {
     const element = document.createElement('div');
-  
+
     // Lodash, currently included via a script, is required for this line to work
     // Lodash, imported by cli
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+    element.innerHTML = _.join(['Hello', 'John', '<br/>'], ' ');
+    element.classList.add('hello');
+
+    const myicon = new Image();
+    myicon.src = icon;
+
+    element.appendChild(myicon);
+
     return element;
-  }
-  
-  document.body.appendChild(component());
+}
+
+document.body.appendChild(component());
